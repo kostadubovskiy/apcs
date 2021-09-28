e22805355fa87abede3e2a29e2f7e46b1db8a0f7
kostadubovskiy <kostadubovskiy@gmail.com>
2021-09-28 19:47:54 -0400

    dir management

---
+++ Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+    public static double isDivisible (int n, int m) {
+        boolean result;
+        if (n % m == 0) {
+            result = true;
+        } else {
+            result = false;
+        }
+        return equals(result);
+    } 
+}
