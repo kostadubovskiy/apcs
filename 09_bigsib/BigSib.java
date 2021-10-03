49d066c5c3df41b6c1124fc1f23b74fbaff15d21
Kosta Dubovskiy <82980954+kostadubovskiy@users.noreply.github.com>
2021-10-03 11:33:59 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,3 +1,10 @@
+/* 
+Kosta Dubovskiy
+APCS
+HW09 -- What are BigSibs good for? — Varied Greetings
+2021-10-04
+*/
+
 public class BigSib {
 	public static String greet (String name) {
 		String result = "Why, hello there, " + name + ". How do you do?";
