cb9e9e4f0219876956478535f49b46cdb196e84e
Kosta Dubovskiy <82980954+kostadubovskiy@users.noreply.github.com>
2021-10-03 11:35:02 -0400

    Update Hello.java

--- pre/01/Hello.java
+++ pre/01/Hello.java
@@ -1,3 +1,10 @@
+/* 
+Kosta Dubovskiy
+APCS
+HW05 -- How was your summer?
+2021-09-28
+*/
+
 public class Hello{
     public static void main(String[] args){
         System.out.println("Hello world!");
