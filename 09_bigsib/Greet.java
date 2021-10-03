9ec393a83b8d36989070cba61a23703d88572c04
Kosta Dubovskiy <82980954+kostadubovskiy@users.noreply.github.com>
2021-10-03 11:33:08 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,3 +1,10 @@
+/* 
+Kosta Dubovskiy
+APCS
+HW09 -- What are BigSibs good for? — Varied Greetings
+2021-10-04
+*/
+
 public class Greet {
 	public static void main( String[] args ) {
 		BigSib.greet("Flim");
