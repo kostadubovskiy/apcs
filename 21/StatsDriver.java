ed27fd9fc2fd298a3875962fc9e0ed93fd2097a3
kostadubovskiy <kostadubovskiy@gmail.com>
2021-10-20 12:38:39 -0400

    21

--- 21/StatsDriver.java
+++ 21/StatsDriver.java
@@ -1,10 +1,11 @@
 public class StatsDriver {
 	public static void main(String[] args) {
-		System.out.println(Stats.mean(15, 20);
-		System.out.println(Stats.mean(20.5, 30.6);
-		System.out.println(Stats.max(15, 40));
-		System.out.println(Stats.max(15.5, 30.2));
-		System.out.println(Stats.geoMean(9, 16);
-		System.out.println(Stats.geoMean(9.5, 16.8);
+		Stats stat = new Stats();
+		System.out.println(stat.mean(15, 20); // 17.5
+		System.out.println(stat.mean(20.5, 31.5); // 26 
+		System.out.println(stat.max(15, 40)); // 40
+		System.out.println(stat.max(15.5, 30.2)); // 30.2
+		System.out.println(stat.geoMean(9, 16); // 12
+		System.out.println(stat.geoMean(9.5, 16.8); // 12.6
 	}
 }
