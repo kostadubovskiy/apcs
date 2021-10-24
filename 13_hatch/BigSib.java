eaa399ae9570d6463fa842cc0bca8e72532c3fef
kostadubovskiy <kostadubovskiy@gmail.com>
2021-10-24 12:19:59 -0400

    hw23 coin.java working commit

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,31 @@
+/* 
+Joseph & Kosta: Featuring Duckie Po and Duckie Davie
+APCS
+HW13 -- BigSib hatch
+2021-10-04
+*/
+
+/* DISCO
+Can't use non-static attribute/method pair in this case
+*/
+
+/* QCC
+
+*/
+
+public class BigSib {
+	public static String helloMsg;
+
+	public BigSib(String newHelloMsg) {
+		helloMsg = newHelloMsg;
+	}
+	
+	public static void setHelloMsg (String newHelloMsg) {
+		helloMsg = newHelloMsg;
+	}
+
+	public static String greet (String greeting) {
+		String result = helloMsg + " " + greeting;
+		return result;
+	}
+}
