315f83cbf0dc0a3f809988a789d4ac4f63af8b32
kostadubovskiy <kostadubovskiy@gmail.com>
2021-11-17 12:17:42 -0500

    test38

---
+++ 38/test.java
@@ -0,0 +1,10 @@
+public class test {
+	public static void main (String[] args) {
+		Integer x = new Integer(3);
+		Integer y = 3;
+		System.out.println(x == y);
+		System.out.println(x.equals(y));
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
