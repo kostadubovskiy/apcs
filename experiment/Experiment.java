11793e194a11fe6e7ef557ac99f9b86db7ab4ffa
kostadubovskiy <kostadubovskiy@gmail.com>
2021-11-29 13:14:21 -0500

    experiment

---
+++ experiment/Experiment.java
@@ -0,0 +1,8 @@
+public class Experiment {
+    public static void main(String[] args) {
+        int v = 10;
+        int tempV = v;
+        v += 5;
+        System.out.println(tempV); // returned 10
+    }
+}
