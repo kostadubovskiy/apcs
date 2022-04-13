063c76fd48466ab6298d465d492fd377de8e8553
kostadubovskiy <kostadubovskiy@gmail.com>
2022-04-13 12:54:40 -0400

    91 start

--- 90_deque/LLDeque.java
+++ 90_deque/LLDeque.java
@@ -6,7 +6,7 @@ time spent: 00.8 hrs */
 
 import java.util.LinkedList;
 
-public class YourImplementationOfDeque<Card> implements Deque<Card>{
+public class LLDeque<Card> implements Deque<Card>{
     //instance vars
     private LinkedList<Card> _deque = new LinkedList<Card>();
 
@@ -25,7 +25,7 @@ public class YourImplementationOfDeque<Card> implements Deque<Card>{
     public boolean isEmpty(){
         return size() == 0;
     }
-    
+
     public void addFirst(Card c){
         _deque.addFirst(c);
     }
@@ -61,47 +61,47 @@ public class YourImplementationOfDeque<Card> implements Deque<Card>{
 
         PirateQueue.addFirst("Pirate"); //expected: Pirate Dread
         System.out.println(PirateQueue.peekFirst()); //expected: Pirate
-        
+
         PirateQueue.addLast("Roberts"); //expected: Pirate Dread Roberts
         System.out.println(PirateQueue.peekLast()); //expected: Roberts
-        
+
         PirateQueue.addLast("Blackbeard"); //expected: Pirate Dread Roberts Blackbeard
         System.out.println(PirateQueue.peekLast()); //expected: Blackbeard
-        
+
         PirateQueue.addFirst("Peter"); //expected: Peter Pirate Dread Roberts Blackbeard
         System.out.println(PirateQueue.peekFirst()); //expected: Peter
-        
+
         PirateQueue.addLast("Stuyvesant"); //expected: Peter Pirate Dread Roberts Blackbeard Stuyvesant
         System.out.println(PirateQueue.peekLast()); //expected: Stuyvesant
-        
+
         System.out.println(PirateQueue.isEmpty()); //expected: false
-        System.out.println(PirateQueue.size()); //expected: 6   
-        
+        System.out.println(PirateQueue.size()); //expected: 6
+
         System.out.println("\nnow testing toString()...");
         System.out.println( PirateQueue ); //for testing toString()...
-        
+
         System.out.println("\nnow dequeuing...");
         System.out.println( PirateQueue.removeFirst() ); //expected: Peter
         System.out.println( PirateQueue ); //expected: Pirate Dread Roberts Blackbeard Stuyvesant
-        
+
         System.out.println( PirateQueue.removeFirst() ); //expected: Pirate
         System.out.println( PirateQueue ); //expected: Dread Roberts Blackbeard Stuyvesant
-        
+
         System.out.println( PirateQueue.removeLast() ); //expected: Stuyvesant
-        System.out.println( PirateQueue ); //expected: Dread Roberts Blackbeard 
-        
+        System.out.println( PirateQueue ); //expected: Dread Roberts Blackbeard
+
         System.out.println( PirateQueue.removeLast() ); //expected: Blackbeard
-        System.out.println( PirateQueue ); //expected: Dread Roberts  
-        
+        System.out.println( PirateQueue ); //expected: Dread Roberts
+
         System.out.println( PirateQueue.removeFirst() ); //expected: Dread
-        System.out.println( PirateQueue ); //expected: Roberts  
-        
+        System.out.println( PirateQueue ); //expected: Roberts
+
         System.out.println( PirateQueue.removeLast() ); //expected: Roberts
-        System.out.println( PirateQueue ); //expected:   
-        
+        System.out.println( PirateQueue ); //expected:
+
         System.out.println(PirateQueue.isEmpty()); //expected: true
         /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
-    
+
       ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
 
   }//end main
