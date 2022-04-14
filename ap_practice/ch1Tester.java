6de843cf55ab0efd483e34d5b44f27b74baf3d71
kostadubovskiy <kostadubovskiy@gmail.com>
2022-04-14 13:21:20 -0400

    cobo practice

---
+++ ap_practice/ch1Tester.java
@@ -0,0 +1,12 @@
+public class ch1Tester {
+  public static void main(String[] args) {
+    System.out.println("Q2");
+    System.out.println("\\* This is not\n a comment *\\");
+    System.out.println("Q3");
+    double answer = 13 / 5;
+    System.out.println(answer);
+    System.out.println("Q4");
+    int result = 13 - 3 * 6 / 4 % 3;
+    System.out.println(result);
+  }
+}
